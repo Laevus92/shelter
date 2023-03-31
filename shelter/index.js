b81e4de3097e1db5e441de7acd0fd679dead4605
Laevus92 <laevus@yandex.ru>
2023-03-31 10:54:33 +0300

    feat: add pop-up menu for tablet and mobile versions of web-pages

--- shelter/index.js
+++ shelter/index.js
@@ -1,14 +1,50 @@
-console.log(`
-    1. Верстка страницы MAIN соответствует макету при ширине экрана 1280px (+14);\n
-    2. Верстка страницы MAIN соответствует макету при ширине экрана 768px (+14);\n
-    3. Верстка страницы MAIN соответствует макету при ширине экрана 320px (+14);\n
-    4. Верстка страницы MAIN соответствует макету при ширине экрана 1280px (+6);\n
-    5. Верстка страницы MAIN соответствует макету при ширине экрана 768px (+6);\n
-    6. Верстка страницы MAIN соответствует макету при ширине экрана 320px (+6);\n
-    7. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется (+20); \n
-    8. Верстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (+8); \n
-    9. При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню (+4); \n
-    10. Верстка обеих страниц валидная (+8).
+const burgerButton = document.querySelector('.burger');
+const backgroundMenu = document.querySelector('.pop-up-background');
+const popUpMenu = document.querySelector('.pop-up-menu');
 
-    total 100
-`)
+burgerButton.addEventListener('click', () => {
+    if (backgroundMenu.classList.contains('active')){
+        backgroundMenu.classList.remove('active');
+    } else {
+        backgroundMenu.classList.add('active');
+    }
+
+    if (popUpMenu.classList.contains('active')){
+        popUpMenu.classList.remove('active');
+    } else {
+        popUpMenu.classList.add('active');
+    }
+    
+    if (burgerButton.style.transform === '' || burgerButton.style.transform === 'rotate(0deg)'){
+        burgerButton.style.transform = 'rotate(90deg)'
+    } else {
+        burgerButton.style.transform = 'rotate(0deg)'
+    }
+
+    if (document.querySelector("body").style.overflow === '' ||
+        document.querySelector("body").style.overflow === 'unset') {
+        document.querySelector("body").style.overflow = 'hidden';
+    } else {
+        document.querySelector("body").style.overflow = 'unset';
+    }
+})
+
+backgroundMenu.addEventListener('click', (click) => {
+    if (click.target === backgroundMenu) {
+        backgroundMenu.classList.remove('active');
+        popUpMenu.classList.remove('active');
+        burgerButton.style.transform = 'rotate(0deg)';
+        document.querySelector("body").style.overflow = 'unset';
+    }
+})
+
+const navLinks = document.querySelectorAll(".pop-up-background > div > ul > li");
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        backgroundMenu.classList.remove('active');
+        popUpMenu.classList.remove('active');
+        burgerButton.style.transform = 'rotate(0deg)';
+        document.querySelector("body").style.overflow = 'unset';
+    })
+})
